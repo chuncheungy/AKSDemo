158cc9dcf9b5d3a1622acf79b7de9a0a3144fe20
Stephen Yau <chuncheungy@gmail.com>
2025-07-06 23:28:34 -0400

    Improve local-exec portability and add missing newlines

--- terraform/modules/2_build/main.tf
+++ terraform/modules/2_build/main.tf
@@ -5,10 +5,14 @@ resource "null_resource" "this" {
   }
 
   provisioner "local-exec" {
-    command = <<EOT
-            docker login ${var.acr_login_server} -u ${var.acr_admin_username} -p ${var.acr_admin_password}
-            docker build -t ${var.acr_login_server}/${var.image_name}:${var.build_version} ${var.app_src_path}
-            docker push ${var.acr_login_server}/${var.image_name}:${var.build_version}
-        EOT
+    interpreter = ["/usr/bin/env", "bash", "-c"]
+    command = <<-EOT
+      set -euo pipefail
+      docker login "${var.acr_login_server}" \
+        -u "${var.acr_admin_username}" \
+        -p "${var.acr_admin_password}"
+      docker build -t "${var.acr_login_server}/${var.image_name}:${var.build_version}" "${var.app_src_path}"
+      docker push "${var.acr_login_server}/${var.image_name}:${var.build_version}"
+    EOT
   }
 }
